0b811d1050595f0dead16b68e3928da2fb9fbbc4
muhammad-haris-dk <142913590+muhammad-haris-dk@users.noreply.github.com>
2024-07-20 01:39:57 -0700

    work Completed

---
+++ index.js
@@ -0,0 +1,31 @@
+require("dotenv").config();
+const cors = require("cors");
+
+const { database_Connect } = require("./database/database_Connect");
+const all_Routes = require("./routes/index");
+
+const express = require("express");
+
+const app = express();
+
+let corsOption = {
+  origin: "*",
+  methods: "GET,POST",
+  credentials: true,
+};
+
+app.get("/", (req, res) => {
+  return res.send("Hello World");
+});
+
+app.use(cors(corsOption));
+
+app.use(express.json());
+
+database_Connect();
+
+app.use(all_Routes);
+
+app.listen(process.env.PORT, () => {
+  console.log("server is runnung", process.env.PORT);
+});
