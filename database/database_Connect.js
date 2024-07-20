9b207ed65021d4ec5469987ccfa267ae4119a157
muhammad-haris-dk <142913590+muhammad-haris-dk@users.noreply.github.com>
2024-07-20 05:02:47 -0700

    done file

--- database/database_Connect.js
+++ database/database_Connect.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 const database_Connect = () => {
-  mongoose.connect(process.env.MONGODB_URL);
+  mongoose.connect(process.env.MONGODB_URI);
 
   mongoose.connection.on("connected", () => {
     console.log("Mongoose connected to database");
